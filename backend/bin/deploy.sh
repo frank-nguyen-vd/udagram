2f45793f854f7876801202a39098bf21e5b0a2b4
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-09-04 14:40:24 +0800

    Set aws credentials in backend scripts

--- backend/bin/deploy.sh
+++ backend/bin/deploy.sh
@@ -1,3 +1,16 @@
+#!/usr/bin/env bash
+AWS_CONFIG_FILE=$HOME/.aws/config
+
+mkdir $HOME/.aws
+touch $AWS_CONFIG_FILE
+chmod 600 $AWS_CONFIG_FILE
+
+echo "[profile eb-cli]"                              > $AWS_CONFIG_FILE
+echo "aws_access_key_id=$AWS_ACCESS_KEY_ID"         >> $AWS_CONFIG_FILE
+echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> $AWS_CONFIG_FILE
+
+
+
 eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 eb setenv AWS_BUCKET=$AWS_BUCKET
 eb setenv AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
