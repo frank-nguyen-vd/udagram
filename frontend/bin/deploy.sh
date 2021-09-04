389ef26a00dcdf6cf29e92abaa77622ac127a9df
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-09-04 15:25:59 +0800

    Update backend script

--- frontend/bin/deploy.sh
+++ frontend/bin/deploy.sh
@@ -4,14 +4,14 @@ AWS_CREDENTIALS=$HOME/.aws/credentials
 mkdir $HOME/.aws
 
 touch $AWS_CONFIG_FILE
-chmod 600 $AWS_CONFIG_FILE
+chmod 777 $AWS_CONFIG_FILE
 
 echo "[profile default]" > $AWS_CONFIG_FILE
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> $AWS_CONFIG_FILE
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> $AWS_CONFIG_FILE
 
 touch $AWS_CREDENTIALS
-chmod 600 $AWS_CREDENTIALS
+chmod 777 $AWS_CREDENTIALS
 
 echo "[default]" > $AWS_CREDENTIALS
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> $AWS_CREDENTIALS
