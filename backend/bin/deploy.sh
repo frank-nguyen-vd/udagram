20ca8bf955c5735c7722349c33556b4131601ef2
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-09-04 15:07:41 +0800

    Set up aws credentials

--- backend/bin/deploy.sh
+++ backend/bin/deploy.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 zip -r ./www/Archive.zip ./www
 
-AWS_CONFIG_FILE=$HOME/.aws/config
-
 mkdir $HOME/.aws
 touch $AWS_CONFIG_FILE
 chmod 600 $AWS_CONFIG_FILE
 
-echo "[profile eb-cli]"                              > $AWS_CONFIG_FILE
-echo "aws_access_key_id=$AWS_ACCESS_KEY_ID"         >> $AWS_CONFIG_FILE
+echo "[profile default]" > $AWS_CONFIG_FILE
+echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> $AWS_CONFIG_FILE
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> $AWS_CONFIG_FILE
 
 eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
--- frontend/bin/deploy.sh
+++ frontend/bin/deploy.sh
@@ -1,1 +1,11 @@
+AWS_CONFIG_FILE=$HOME/.aws/config
+
+mkdir $HOME/.aws
+touch $AWS_CONFIG_FILE
+chmod 600 $AWS_CONFIG_FILE
+
+echo "[profile ]" > $AWS_CONFIG_FILE
+echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> $AWS_CONFIG_FILE
+echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> $AWS_CONFIG_FILE
+
 aws s3 cp --recursive --acl public-read ./www s3://udagram-web/
